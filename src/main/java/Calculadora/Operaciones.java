82709302caf79e6230e1fddfb89b055f40782798
Luis Franco <lmfg06@gmail.com>
2024-09-02 19:10:24 -0500

    Main Commit

---
+++ src/main/java/Calculadora/Operaciones.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Calculadora;
+
+/**
+ *
+ * @author lmfg0
+ */
+public class Operaciones {
+
+    public Operaciones() {
+    }
+    
+    public double Addition(double a, double b){
+        return a + b;
+    }
+    
+    public double Subtraction(double a, double b){
+        return a - b;
+    }
+    
+    public double Multiplication(double a, double b){
+        return a * b;
+    }
+    
+    public double Divided(double a, double b){
+        return a / b;
+    }    
+}
